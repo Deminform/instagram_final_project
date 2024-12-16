5b884c49246ae2c04c713e15c13de585b707c1cc
Deminform <deminform@gmail.com>
2024-12-16 06:41:24 +0100

    0.1.4 Routes added

--- src/posts/routes.py
+++ src/posts/routes.py
@@ -24,20 +24,74 @@ from fastapi_limiter.depends import RateLimiter
 from jose import JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from conf import messages
 from database.db import get_db
-from src.posts.schemas import PostResponseSchema
+from src.posts.schemas import PostResponseSchema, PostSchema, PostUpdateSchema
+from src.posts import repository as posts_repository
+from src.users.models import User
+from src.services.auth import auth_service
+from src.images import repository as images_repository
 
 router = APIRouter(prefix="/posts", tags=["posts"])
 
 
-@router.get("/", response_model=list[PostResponseSchema], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=list[PostResponseSchema])
 async def get_post_by_filter(
         limit: int = Query(10, ge=10, le=100),
         offset: int = Query(0, ge=0),
         user_id: int | None = Query(None, description="Get posts by user id"),
-        # credentials: HTTPAuthorizationCredentials = Security(get_refresh_token),
-        db: AsyncSession = Depends(get_db)):
+        db: AsyncSession = Depends(get_db)
+        # user: User = Depends(auth_service.get_current_user),
+):
+    posts = await posts_repository.get_post_by_filter(db, user_id, limit, offset)
+    return posts
+
+@router.get("/{post_id}", response_model=PostResponseSchema)
+async def get_post_by_id(
+        post_id: int,
+        db: AsyncSession = Depends(get_db),
+        # user: User = Depends(auth_service.get_current_user)
+):
+    post = await posts_repository.get_post_by_id(db, post_id)
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=messages.POST_NOT_FOUND
+        )
+    return post
+
+
+
+@router.post("/", response_model=PostResponseSchema)
+async def create_post(
+        body: PostSchema,
+        image: UploadFile = File(...),
+        db: AsyncSession = Depends(get_db),
+        user: User = Depends(auth_service.get_current_user),
+):
+    image = await images_repository.create_image(db, image)
+    post = await posts_repository.create_post(db, user, body, image)
+    return post
+
+
+@router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(
+        post_id: int,
+        db: AsyncSession = Depends(get_db),
+        # user: User = Depends(auth_service.get_current_user),
+):
+
+
+
+@router.put("/{post_id}", response_model=PostResponseSchema)
+async def edit_post(
+        post_id: int,
+        body: PostUpdateSchema,
+        db: AsyncSession = Depends(get_db),
+        # user: User = Depends(auth_service.get_current_user),
+):
     ...
+
+
 
         # - get_post_by_filter()
         # - get_post_by_id()
