0cb961028c3eb5ce761e4dca9bca8cebde797443
Deminform <deminform@gmail.com>
2024-12-16 06:10:13 +0100

    0.1.3 Fix models

--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, DateTime, func, ForeignKey, Float
+from sqlalchemy import Integer, String, DateTime, func, ForeignKey, Float, Boolean
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -8,21 +8,22 @@ from src.posts.models import Post
 
 class User(Base):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     first_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[str] = mapped_column(String(50))
     phone: Mapped[str] = mapped_column(String(15))
     email: Mapped[str] = mapped_column(String(70), unique=True, index=True)
-    username: Mapped[str] = mapped_column(String(50), unique=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True)
     posts: Mapped['Post'] = relationship('Post', backref='users', lazy='joined')
     avatar_url: Mapped[str] = mapped_column(String(255), nullable=True)
-    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('roles.id'), nullable=False)
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('roles.id'))
+    role: Mapped[str] = relationship("Role", lazy="selectin")
+    password: Mapped[str] = mapped_column(String(255))
     refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[DateTime] = mapped_column('created_at', DateTime, default=func.now())
     updated_at: Mapped[DateTime] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
-    is_banned: Mapped[bool] = mapped_column(default=False)
-    is_confirmed: Mapped[bool] = mapped_column(default=False)
+    is_banned: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
 
 
 
