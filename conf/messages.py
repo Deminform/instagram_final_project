1a9fd896fe672c4e7a45bc8c8380253f82eeb3be
Deminform <deminform@gmail.com>
2024-12-22 01:12:29 +0100

    0.1.8 Post-service update

--- conf/messages.py
+++ conf/messages.py
@@ -8,11 +8,12 @@ VERIFICATION_FAILED = 'Verification failed'
 FORBIDDEN = "You do not have permission to perform this action"
 INTERNAL_SERVER_ERROR = 'Internal Server Error'
 BANNED = 'You are banned'
-POST_ALREADY_EXISTS = 'Contact already exists'
-POST_NOT_FOUND = 'Contact not found'
+POST_ALREADY_EXISTS = 'Post already exists'
+POST_NOT_FOUND = 'Post not found'
 USER_NOT_FOUND = 'User not found'
 INVALID_SCOPE_TOKEN = 'Invalid scope token'
 DATABASE_IS_NOT_AVAILABLE = "Database is not available"
 DATABASE_IS_HEALTHY = "Database is healthy"
+DATA_INTEGRITY_ERROR = "Data integrity error"
 
 # TODO REPLACE ALL ERROR MESSAGES IN PROJECT
