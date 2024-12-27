ad852fc6cf5ca18a20879204854d872753452466
Deminform <deminform@gmail.com>
2024-12-27 12:28:02 +0100

    0.1.19 Testcases added

--- src/posts/repository.py
+++ src/posts/repository.py
@@ -12,41 +12,7 @@ class PostRepository:
     def __init__(self, db: AsyncSession):
         self.session = db
 
-    async def get_posts(self, limit: int, offset: int, keyword: str, tag: str):
-        stmt = select(Post)
-        conditions = []
-
-        if tag:
-            conditions.append(Post.tags.any(Tag.name.ilike(f"%{tag}%")))
-        if keyword:
-            conditions.append(Post.description.ilike(f"%{keyword}%"))
-        if conditions:
-            stmt = stmt.where(and_(*conditions))
-
-        stmt = stmt.options(selectinload(Post.tags)).offset(offset).limit(limit)
-
-        posts = await self.session.execute(stmt)
-        return posts.scalars().all()
-
-    async def get_post_by_id(self, post_id: int):
-        stmt = select(Post).where(Post.id == post_id)
-        post = await self.session.execute(stmt)
-        return post.scalars().unique().one_or_none()
-
-    async def delete_post(self, post: Post):
-        await self.session.delete(post)
-        await self.session.commit()
-        return post
-
-    async def update_post_description(self, post: Post, description: str):
-        post.description = description
-        await self.session.commit()
-        await self.session.refresh(post)
-        return post
-
-    async def create_post(
-        self, user: User, description: str, tags: set[Tag], images: dict
-    ):
+    async def create_post(self, user: User, description: str, tags: set[Tag], images: dict):
         post = Post(
             description=description,
             user_id=user.id,
@@ -57,4 +23,37 @@ class PostRepository:
         self.session.add(post)
         await self.session.commit()
         await self.session.refresh(post)
+        return post
+
+
+    async def get_post_by_id(self, post_id: int):
+        stmt = select(Post).where(Post.id == post_id)
+        post = await self.session.execute(stmt)
+        return post.scalars().unique().one_or_none()
+
+
+    async def get_posts(self, limit: int, offset: int, keyword: str, tag: str):
+        stmt = select(Post)
+        conditions = []
+        if tag:
+            conditions.append(Post.tags.any(Tag.name.ilike(f"%{tag}%")))
+        if keyword:
+            conditions.append(Post.description.ilike(f"%{keyword}%"))
+        if conditions:
+            stmt = stmt.where(and_(*conditions))
+        stmt = stmt.options(selectinload(Post.tags)).offset(offset).limit(limit)
+        posts = await self.session.execute(stmt)
+        return posts.scalars().all()
+
+
+    async def update_post_description(self, post: Post, description: str):
+        post.description = description
+        await self.session.commit()
+        await self.session.refresh(post)
+        return post
+
+
+    async def delete_post(self, post: Post):
+        await self.session.delete(post)
+        await self.session.commit()
         return post
