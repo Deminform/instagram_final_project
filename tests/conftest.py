0d5952ca28a52159df5fd27638a4870cb9ea084e
marinasemak <marina.semak@gmail.com>
2024-12-29 09:22:35 +0200

    Revert "temp changes in conftest"
    This reverts commit 26cebc9c590e23969b8cf65708832ff568cd4515.
    
    	modified:   tests/test_auth

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,24 @@
-import asyncio
-from datetime import datetime
-import logging
+import io
+from io import BytesIO
+from PIL import Image
 
+import asyncio
 import pytest
 import pytest_asyncio
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 from main import app
-from src.users.models import User, Role, Token
+from src.users.models import User, Role
 from src.services.auth.auth_service import Hash, create_access_token, create_refresh_token
 from conf.config import Base, app_config
 from database.db import get_db
 from src.users.schemas import RoleEnum
+from src.users.repository import TokenRepository
 
-TEST_DB_URL = app_config.DATABASE_TEST_URL
-engine = create_async_engine(TEST_DB_URL, echo=False)
-AsyncSessionLocal = async_sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    expire_on_commit=False,
-    bind=engine,
-    class_=AsyncSession,
-)
+
+# logging.basicConfig(level=logging.DEBUG)
+# logger = logging.getLogger("test_logger")
+
 
 test_user_dict = {
         "first_name": "Steve",
@@ -36,6 +33,15 @@ test_user_dict = {
         "is_confirmed":True,
 }
 
+engine = create_async_engine(app_config.TEST_DB_URL, echo=False)
+AsyncSessionLocal = async_sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    bind=engine,
+    class_=AsyncSession,
+)
+
 
 @pytest.fixture(scope="session")
 def event_loop():
@@ -48,15 +54,13 @@ def event_loop():
 @pytest_asyncio.fixture(scope="function")
 async def setup_db():
     async with engine.begin() as conn:
-        # Drop all tables
         await conn.run_sync(Base.metadata.drop_all)
-        # Create tables
         await conn.run_sync(Base.metadata.create_all)
 
     yield
-    # Drop all tables after test
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+
+    # async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -68,10 +72,8 @@ async def db_session(setup_db):
 @pytest_asyncio.fixture(scope="function")
 def override_get_db():
     async def _get_db():
-        logging.info("Opening DB session")
         async with AsyncSessionLocal() as session:
             yield session
-        logging.info("Closing DB session")
 
     app.dependency_overrides[get_db] = _get_db
     yield
@@ -80,24 +82,38 @@ def override_get_db():
 
 @pytest_asyncio.fixture(scope="function")
 async def user_password(faker):
-    return faker.password()
+    password = faker.password()
+    return password
 
 
 @pytest_asyncio.fixture(scope="function")
-async def user_role(db_session) -> Role:
-    role = Role(
-        id=1,
-        name=RoleEnum.USER.value,
-    )
-    db_session.add(role)
+async def file_fixture():
+    file_content = b"This is a test image content"
+    fake_file = BytesIO(file_content)
+    fake_file.name = "test_image.jpg"
+    return fake_file
+
+
+@pytest_asyncio.fixture(scope="function")
+async def user_role(db_session):
+    role_1 = Role(id=1, name=RoleEnum.USER.value)
+    role_2 = Role(id=2, name=RoleEnum.MODER.value)
+    role_3 = Role(id=3, name=RoleEnum.ADMIN.value)
+
+    db_session.add(role_1)
+    db_session.add(role_2)
+    db_session.add(role_3)
     await db_session.commit()
-    await db_session.refresh(role)
-    return role
+    await db_session.refresh(role_1)
+    await db_session.refresh(role_2)
+    await db_session.refresh(role_3)
+
 
 
 @pytest_asyncio.fixture(scope="function")
-async def test_user(db_session, faker, user_password, user_role):
+async def test_user(db_session, user_password, user_role):
     hashed_password = Hash().get_password_hash(user_password)
+
     user = User(
         first_name=test_user_dict["email"],
         last_name=test_user_dict["last_name"],
@@ -105,41 +121,30 @@ async def test_user(db_session, faker, user_password, user_role):
         username=test_user_dict["username"],
         email=test_user_dict["email"],
         avatar_url=test_user_dict["avatar_url"],
-        role_id=1,
+        role_id=3,
         password=hashed_password,
         is_banned=False,
-        is_confirmed=True,
-    )
+        is_confirmed=True)
+
     db_session.add(user)
     await db_session.commit()
     await db_session.refresh(user)
     return user
 
+
 @pytest_asyncio.fixture(scope="function")
-async def auth_header(db_session, test_user: User):
-    access_token = create_access_token({"sub": test_user.email})
-    refresh_token = create_refresh_token({"sub": test_user.email})
-    token = Token(
-        user_id=1,
-        access_token=access_token,
-        refresh_token=refresh_token,
-        is_active=True
-    )
-    db_session.add(token)
-    await db_session.commit()
-    await db_session.refresh(token)
-
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-        # "X-Refresh-Token": refresh_token
-    }
-    return headers
-
-# @pytest_asyncio.fixture(scope="function")
-# async def get_user_tokens(db_session):
-#         access_token = create_access_token(data={"sub": test_user_dict['email']})
-#         refresh_token = create_refresh_token(data={"sub": test_user_dict['email']})
-#         await TokenRepository(db_session).add_tokens(1, access_token, refresh_token, True)
-#         return {"access_token": access_token, "refresh_token": refresh_token}
+async def get_user_tokens(db_session):
+        access_token = create_access_token(data={"sub": test_user_dict['email']})
+        refresh_token = create_refresh_token(data={"sub": test_user_dict['email']})
+        await TokenRepository(db_session).add_tokens(1, access_token, refresh_token, True)
+        return {"access_token": access_token, "refresh_token": refresh_token}
 
 
+@pytest_asyncio.fixture(scope="function")
+async def file_fixture():
+    img = Image.new('RGB', (100, 100), color=(0, 0, 0))
+    fake_file = io.BytesIO()
+    img.save(fake_file, format='JPEG')
+    fake_file.seek(0)
+    fake_file.name = "test_image.jpg"
+    return fake_file
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,5 @@
 import pytest
-from unittest.mock import patch, AsyncMock
+from unittest.mock import patch
 from fastapi import BackgroundTasks
 from httpx import AsyncClient, ASGITransport
 
@@ -7,34 +7,34 @@ from main import app
 from src.users.models import Role
 
 
+@pytest.mark.asyncio
+async def test_user_registration(user_role: Role, override_get_db, faker):
+    with patch.object(BackgroundTasks, "add_task"):
+        async with AsyncClient(
+            transport=ASGITransport(app=app), base_url="http://test"
+        ) as ac:
+            payload = {
+                "first_name": faker.first_name(),
+                "last_name": faker.last_name(),
+                "phone": faker.phone_number()[:15],
+                "username": faker.user_name(),
+                "email": faker.email(),
+                "password": faker.password(),
+            }
+            response = await ac.post(
+                "/api/auth/signup",
+                json=payload,
+            )
+            assert response.status_code == 201
+            data = response.json()
+            assert data["email"] == payload["email"]
+            assert data["username"] == payload["username"]
+            assert data.get("password") is None
+            assert data["id"] is not None
+
+
 # @pytest.mark.asyncio
-# async def test_user_registration(user_role: Role, override_get_db, faker):
-#     with patch.object(BackgroundTasks, "add_task"):
-#         async with AsyncClient(
-#             transport=ASGITransport(app=app), base_url="http://test"
-#         ) as ac:
-#             payload = {
-#                 "first_name": faker.first_name(),
-#                 "last_name": faker.last_name(),
-#                 "phone": faker.phone_number()[:15],
-#                 "username": faker.user_name(),
-#                 "email": faker.email(),
-#                 "password": faker.password(),
-#             }
-#             response = await ac.post(
-#                 "/api/auth/signup",
-#                 json=payload,
-#             )
-#             assert response.status_code == 201
-#             data = response.json()
-#             assert data["email"] == payload["email"]
-#             assert data["username"] == payload["username"]
-#             assert data.get("password") is None
-#             assert data["id"] is not None
-#
-#
-# @pytest.mark.asyncio
-# async def test_user_login(override_get_db, test_user, user_password):
+# async def user_login(override_get_db, test_user, user_password):
 #     async with AsyncClient(
 #         transport=ASGITransport(app=app), base_url="http://test"
 #     ) as ac:
@@ -50,23 +50,3 @@ from src.users.models import Role
 #         data = response.json()
 #         assert "access_token" in data
 #         assert "refresh_token" in data
-
-
-@pytest.mark.asyncio
-async def test_get_user_by_id(
-    override_get_db, test_user, auth_header
-):
-
-    async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test"
-    ) as ac:
-        print(f"Request Headers: {auth_header}")
-        # Then, retrieve the user by ID
-        response = await ac.get(
-            f"/api/users/{test_user.id}", headers=auth_header
-        )
-
-    assert response.status_code == 200
-    # data = response.json()
-    # assert data["first_name"] == test_user.first_name
-    # assert data["last_name"] == test_user.last_name
