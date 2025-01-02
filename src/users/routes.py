e3ca2db8e199dbe7e0bd8c693fcac1e16be95321
marinasemak <marina.semak@gmail.com>
2025-01-02 22:29:07 +0200

    add users search/filter

--- src/users/routes.py
+++ src/users/routes.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, File, UploadFile, status
+from typing import Sequence
+
+from fastapi import APIRouter, Depends, File, UploadFile, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.db import get_db
@@ -98,3 +100,18 @@ async def change_user_role(
     user_service = UserService(db)
     await user_service.change_role(user_id, role)
     return {"message": "Success"}
+
+
+@router_admin.get("/search", response_model=Sequence[UserResponse], status_code=status.HTTP_200_OK)
+async def search_users(
+        search_param: str = Query(None, description="Search by first name or last name or email"),
+        has_posts: bool = True,
+        offset: int = Query(default=0, ge=0),
+        limit: int = Query(default=10, le=100, ge=10),
+        current_user: User = Depends(RoleChecker([RoleEnum.ADMIN, RoleEnum.MODER])),  # TODO Check admin permission
+        db: AsyncSession = Depends(get_db),
+):
+    user_service = UserService(db)
+    users = await user_service.search_users(search_param, has_posts, offset, limit)
+    return users
+
