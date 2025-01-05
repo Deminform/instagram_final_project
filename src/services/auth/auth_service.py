3d1d6eac501e028b61c461699dea8094914f7356
marinasemak <marina.semak@gmail.com>
2025-01-05 20:01:57 +0200

    fix 401 error details

--- src/services/auth/auth_service.py
+++ src/services/auth/auth_service.py
@@ -25,6 +25,7 @@ class UnauthorizedException(HTTPException):
     def __init__(self, detail: str):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=detail,
             headers={"WWW-Authenticate": "Bearer"},
         )
 
